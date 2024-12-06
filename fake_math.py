a0b151e8589f91a9ed4af78978c0ac26a56f1156
anytazzzz <igorcernienko30@gmail.com>
2024-12-07 00:42:59 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def divide(first, second):
+    #first = int(input("Введите делимое: "))
+   # second = int(input("Введите делитель: "))
+    if second == 0:
+        print ("Ошибка")
+    else:
+        return first / second
+
+
+
